b43330b5dce84188c9fd2689ff8d8f0c025f01c9
Iandraina44 <iandrainaarivonjy@gmail.com>
2025-01-30 11:03:55 +0300

    hard reset changing package name

--- src/exceptions/NumericException.java
+++
@@ -1,6 +0,0 @@
-package exceptions;
-public class NumericException extends Exception {
-    public NumericException(String paramName) {
-        super("Le parametre " + paramName + " doit etre un nombre");
-    }
-}
